5a6012b05df253b7cf12c35087aed956f6ea4ac4
valentin poli <valentin.poli@epita.com>
2023-03-16 11:43:49 +0100

    yes

--- GPU/tp2/ex1_image_processing.cu
+++ GPU/tp2/ex1_image_processing.cu
@@ -12,15 +12,28 @@ inline void cuda_check(cudaError_t code, const char *file, int line) {
 
 template <typename T>
 __device__ inline T* get_ptr(T *img, int i, int j, int C, size_t pitch) {
-    // ...
-	return 0;
+	return img[j*pitch + i - (j*pitch + i)%C];
 }
 
 
 __global__
 void process(int N, int M, int C, int pitch, float* img)
 {
-    // ...
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    if (i < n && j < m) {
+        *float pixel = get_ptr(img,i,j,C,pitch);
+        float newColor = 0;
+        for (k=0; k<C; k=+1)
+        {
+            newColor += pixel[k];
+        }
+        newColor =  newColor/C
+        for (k=0; k<C; k=+1)
+        {
+            pixel[k] = newColor
+        }
+    }
 }
 
 
@@ -28,7 +41,6 @@ int main(int argc, char const *argv[])
 {
     const std::string filename = argc >= 2 ? argv[1] : "image.jpg";
     std::cout << "filename = " << filename << std::endl;
-
     int M = 0;
     int N = 0;
     int C = 0;
@@ -37,10 +49,23 @@ int main(int argc, char const *argv[])
     std::cout << "M (rows, height) = " << M << std::endl;
     std::cout << "C (channels, depth) = " << C << std::endl;
 
-    // ...
+    size_t pitch; // combien dois faire l'alignement ?
 
+    float* cpy;
+    cudaMallocPitch(cpy, &pitch, N * sizeof(float), M);
+    cudaMemcpy2D(cpy, pitch, img, N * sizeof(float), N * sizeof(float), M, cudaMemcpyHostToDevice);
+
+    // launch kernel
+    dim3 block_dim(32, 32);
+    dim3 grid_dim((M + block_dim.x - 1) / block_dim.x, (N + block_dim.y - 1) / block_dim.y);
+    process<<<grid_dim, block_dim>>>(N,M,C,pitch,cpy);
+    
+    // copy device memory back to host memory
+    cudaMemcpy2D(img, M * sizeof(float), cpy, pitch, N * sizeof(float), M, cudaMemcpyDeviceToHost);
+    
     image::save("result.jpg", N, M, C, img);
 
+    cudaFree(cpy);
     free(img);
 
     return 0;
