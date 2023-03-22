8712a2b71a74d62849a76ae377c4b3461798e752
valentin.poli <valentin.poli@epita.fr>
2023-03-23 00:25:58 +0100

    yes

--- GPU/tp2/ex3_reduction.cu
+++ GPU/tp2/ex3_reduction.cu
@@ -19,7 +19,7 @@ __global__ void dot(int n, const float *x, const float *y, float* res)
     //buffer[blockDim.x] = 0;
     for (int j = i; j < n; j += block_dim*block_count) {
 
-        printf("%d\n", blockDim.x);
+        printf("%d\n", blockIdx.x);
         buffer[blockDim.x] += y[j] * x[j];
         //printf("on fait la multiplication %f * %f = %f et le buffer egal a %f dans le bloc %d\n",y[j],x[j],y[j] * x[j], buffer[blockIdx.x], blockIdx.x);
     }
