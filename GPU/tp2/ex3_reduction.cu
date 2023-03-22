7f45f78f57a7756b6d2f6f39a32886db90290dc3
valentin.poli <valentin.poli@epita.fr>
2023-03-22 23:01:00 +0100

    yes

--- GPU/tp2/ex3_reduction.cu
+++ GPU/tp2/ex3_reduction.cu
@@ -19,7 +19,7 @@ __global__ void dot(int n, const float *x, const float *y, float* res)
     buffer[blockIdx.x * blockDim.x + threadIdx.x] = 0;
     for (int j = i; j < n; j += block_dim*block_count) {
         buffer[blockIdx.x * blockDim.x + threadIdx.x] += y[j] * x[j];
-        printf("on fait la multiplication %f * %f = %f et le buffer egal a %f dans le bloc %d\n",y[j],x[j],y[j] * x[j], buffer[blockIdx.x * blockDim.x + threadIdx.x], blockIdx.x * blockDim.x + threadIdx.x);
+        printf("on fait la multiplication %f * %f = %f et le buffer egal a %f dans le bloc %d\n",y[j],x[j],y[j] * x[j], buffer[blockIdx.x * blockDim.x + threadIdx.x], i);
     }
     printf("this is the block %d and the total for it is %f\n",blockIdx.x * blockDim.x + threadIdx.x,buffer[blockIdx.x * blockDim.x + threadIdx.x]);
     __syncthreads();
