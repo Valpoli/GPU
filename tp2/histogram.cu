a76de64d1be9529890b52784ed54b2997f0d7172
Valpoli <valentin.poli@epita.fr>
2023-11-02 10:59:27 +0100

    yes

--- tp2/histogram.cu
+++ tp2/histogram.cu
@@ -9,7 +9,18 @@ inline void cuda_check(cudaError_t code, const char *file, int line) {
 
 #define BINS 32
 
-
+void complet_hist_cpu(float *img, int N, int M, int C, int *hist)
+{
+    for (int i = 0; i < N * M * C; ++i)
+    {
+        float pixel = img[i];
+        if (pixel < 1)
+        {
+            int idx = pixel * BINS
+            ++ hist[idx]
+        }
+    }
+}
 
 int main()
 {
@@ -17,7 +28,13 @@ int main()
     float* img = image::load("mandelbrot.jpg", &N, &M, &C, 1);
     const int size = N * M * C;
 
-    // ...
+    int *hist = (int) malloc(sizeof(int) * BINS);
+    complet_hist_cpu(img, N, M, C, hist);
+    for (int i = 0; i < N * M * C; ++i)
+    {
+        cout << hist[i] << endl;
+    }
+    free(hist)
 
     return 0;
 }
