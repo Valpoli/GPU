2b32dd25c346fc8527818e595905a3a7821960cc
Valentin Poli <valentin.poli@epita.fr>
2023-11-21 16:04:54 +0100

    yes

--- tp4/main.cu
+++ tp4/main.cu
@@ -15,14 +15,47 @@ std::vector<int> scan_exclu(std::vector<int> table)
     std::size_t size = table.size();
     std::vector<int> res = make_vector((int) size);
     res[0] = 0;
-    for (std::size_t i = 1; i < size; i++)
+    for (std::size_t i = 1; i < size; ++i)
     {
         res[i] = res[i-1] + table[i-1];
     }
     return res;
 }
 
-
+__global__ void scan1(int* x, int N)
+{
+    __shared__ int buffers[2*N];
+    int in = 0;
+    int out = N;
+    int i = threadIdx.x;
+    buffers[i] = x[i];
+    __syncthreads();
+    if (i == 0)
+    {
+        x[i] = 0;
+    }
+    else
+    {
+        int step = (int) log2(N);
+        for (int n = 0; n < step; ++n)
+        {
+            offset = 2^step ;
+            if (offset <= i)
+            {
+                buffers[i + out] = buffer[i + in] + buffer[i + in - offset];
+            }
+            else
+            {
+                buffers[i + out] = buffer[i + in];
+            }
+            syncthreads();
+            int temp = in;
+            in = out;
+            out = in;
+        }
+        x[i] = buffers[i + out];
+    }
+}
 
 
 
