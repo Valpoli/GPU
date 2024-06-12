9de829467abd1a3b5fc367c8601b2bd25baeb038
Valpoli <valentin.poli@epita.fr>
2024-06-12 10:40:57 +0200

    yes

--- SujetSources/sources/exercice.cu
+++ SujetSources/sources/exercice.cu
@@ -65,17 +65,52 @@ float* generate1(int N, int M, int C)
     return res;
 }
 
+__device__ bool is_converging_greyscale(float a, float b)
+{
+    float zc = -0.5;
+    float z_imc = 0.6;
+    float z = a;
+    float z_im = b;
+    int i = 0;
+    while (i <= 100) {
+        float tempz = z * z - z_im * z_im + zc;
+        float tempz_im = 2.0 * z * z_im + z_imc;
+        z = tempz;
+        z_im = tempz_im;
+        if (sqrt(z*z +z_im*z_im)*sqrt(z*z +z_im*z_im) >= 2.0) {
+            break;
+        }
+        i += 1;
+    }
+    return (float)i/iter;
+}
+
 __global__
 void kernel_generate2(int N, int M, int C, int pitch, float* img)
 {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    if (i < N && j < M) {
+        float a, b;
+        map(N, M, i, j, &a, &b);
+        float *pixel = get_ptr(img, i, j, C, pitch);
+        pixel[0] = is_converging(a, b)
+    }
 }
 
 float* generate2(int N, int M, int C)
 {
-    return nullptr;
+    size_t pitch;
+    float* img;
+    cudaMallocPitch(&img, &pitch, N * C * sizeof(float), M);
+    dim3 block_dim(BLOCK_SIZE, BLOCK_SIZE, 1);
+    dim3 grid_dim((N + BLOCK_SIZE - 1) / BLOCK_SIZE, (M + BLOCK_SIZE - 1) / BLOCK_SIZE, 1);
+    kernel_generate1<<<grid_dim, block_dim>>>(N,M,C,pitch,img);
+    float* res =(float*) malloc(M * C * N * sizeof(float));
+    cudaMemcpy2D(res, C * N * sizeof(float), img, pitch, C * N * sizeof(float), M, cudaMemcpyDeviceToHost);
+    cudaFree(img);
+    return res;
 }
-
-
 
 __global__
 void kernel_generate3(int N, int M, int C, int pitch, float* img)
