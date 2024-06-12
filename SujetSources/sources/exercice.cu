92d81874e23fcd2bf965db6c843f0b10dd9208f0
Valpoli <valentin.poli@epita.fr>
2024-06-12 11:53:36 +0200

    yes

--- SujetSources/sources/exercice.cu
+++ SujetSources/sources/exercice.cu
@@ -115,12 +115,52 @@ float* generate2(int N, int M, int C)
     return res;
 }
 
+__device__ void color_from_ratio(float ratio, float* r, float* g, float* b)
+{
+    if (ratio >= 1.0f) {
+        *r = 1.0f;
+        *g = 0.0f;
+        *b = 0.0f;
+    } else if (ratio <= 0.0f) {
+        *r = 0.0f;
+        *g = 0.0f;
+        *b = 1.0f;
+    } else if (ratio <= 0.5f) {
+        *r = 0.0f;
+        *g = ratio * 2.0f;
+        *b = 1.0f - ratio * 2.0f;
+    } else {
+        *r = (ratio - 0.5f) * 2.0f;
+        *g = 1.0f - (ratio - 0.5f) * 2.0f;
+        *b = 0.0f;
+    }
+}
+
 __global__
 void kernel_generate3(int N, int M, int C, int pitch, float* img)
 {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    if (i < N && j < M) {
+        float a, b;
+        map(N, M, i, j, &a, &b);
+        float *pixel = get_ptr(img, i, j, C, pitch);
+        float ratio = is_converging_greyscale(a, b);
+        color_from_ratio(ratio, &pixel[0], &pixel[1], &pixel[2]);
+    }
 }
 
 float* generate3(int N, int M, int C)
 {
-    return nullptr;
+    size_t pitch;
+    float* img;
+    cudaMallocPitch(&img, &pitch, N * C * sizeof(float), M);
+    dim3 block_dim(BLOCK_SIZE, BLOCK_SIZE, 1);
+    dim3 grid_dim((N + BLOCK_SIZE - 1) / BLOCK_SIZE, (M + BLOCK_SIZE - 1) / BLOCK_SIZE, 1);
+    kernel_generate3<<<grid_dim, block_dim>>>(N,M,C,pitch,img);
+    float* res =(float*) malloc(M * C * N * sizeof(float));
+    cudaMemcpy2D(res, C * N * sizeof(float), img, pitch, C * N * sizeof(float), M, cudaMemcpyDeviceToHost);
+    cudaFree(img);
+    return res;
 }
+
